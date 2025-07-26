f93d2a3c622a2c5dba7c784cf4ae01cfd4cc01da
Abhiraj Vaidya <abhivaidya57@gmail.com>
2025-07-26 12:22:09 +0530

    added cors nmiddleware

--- api/main.py
+++ api/main.py
@@ -3,8 +3,17 @@ from fastapi.responses import JSONResponse
 from api.convert import process_image
 from fastapi.staticfiles import StaticFiles
 import os
-
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # or restrict to ["https://snapembed.vercel.app"]
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
